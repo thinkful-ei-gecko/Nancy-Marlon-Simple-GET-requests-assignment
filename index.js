9f57ce41d59df41ab254ba5466e196b22fea34b4
nphiukhao <nphiukhao@gmail.com>
2019-08-22 11:36:10 -0700

    showed multi pics of dogs

--- index.js
+++ index.js
@@ -1,57 +1,38 @@
 'use strict';
 
-function getValue(){
-  $('form').submit(event => {
-    event.preventDefault();
-    const numDogPics = $('.numOfDogPic').val()
-    console.log(numDogPics);
-    return numDogPics;
-  });
-}
-
-function appendFetchHTML(){
-
-  let fetchLink = 'https://dog.ceo/api/breeds/image/random';
-  let newFetchLink = `${fetchLink}/${getValue()}`;
-  console.log('getValue()');
-  console.log(newFetchLink);
-  return newFetchLink;
-}
-
-function getDogImage() {
-  
-  fetch(appendFetchHTML())
+function getDogImage(num) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
+
 function displayResults(responseJson) {
-  console.log(responseJson);
+  
+  $('.results').empty();
   //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
-  );
+  for (let i=0; i < responseJson.message.length; i++){
+    $('.results').append(
+      `<img src="${responseJson.message[i]}" class="results-img">`
+    );
+
+  }
+ 
   //display the results section
-  $('.results').removeClass('hidden');
+  //$('.results').removeClass('hidden');
 }
 
-
-/*function watchForm() {
+function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    getValue();
-    getDogImage();
+    const userInput = $('#numOfDogs').val();
+    getDogImage(userInput);
   });
-} */
+}
 
-// $(function() {
-//   console.log('App loaded! Waiting for submit!');
-//   watchForm();
-// });
-
-getValue();
-$(getValue);
-getDogImage();
-appendFetchHTML();
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
